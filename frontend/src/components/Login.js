8920b96b723286f95c1e47c626f492172bfbffc7
cruedo <74605828+cruedo@users.noreply.github.com>
2021-12-26 15:42:49 +0530

    included event fetch res message

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,24 +4,28 @@ import axios from 'axios'
 export default function Login() {
     const [Username, setUsername] = useState("")
     const [Password, setPassword] = useState("")
+    const [Message, setMessage] = useState("")
 
     function handleSubmit(e) {
-        alert(1)
+        // alert(1)
         e.preventDefault()
         // MAKE Request here
         let creds = {"username": Username, "password": Password}
         axios.post("/login", creds, {"Content-Type": "application/json"})
         .then(res => {
             console.log(res.data)
+            setMessage(res.data.message)
         })
         .catch(error => {
             console.log(error)
+            setMessage(error.response.data.message)
         })
     }
 
     return (
         <div>
             <br/><br/><br/><br/>
+            <b>{Message}</b>
             <form onSubmit={handleSubmit}>
                 <label>
                     Username <br/>
