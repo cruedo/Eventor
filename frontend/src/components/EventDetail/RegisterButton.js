caeaeb956413588e3347ca6e28695771bed81328
cruedo <74605828+cruedo@users.noreply.github.com>
2022-03-01 00:07:38 +0530

    added register button on event detail page (non functional)

---
+++ frontend/src/components/EventDetail/RegisterButton.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom"
+import "./EventDetail.css"
+import imag from "../../static/stock-media.jpg"
+import RightDetail from "./RightDetail"
+import dp from "../../static/dp.jpg"
+
+export default function RegisterButton(props) {
+
+
+    return (
+        // <div className="reg-btn-div">
+            <button className="right-card reg-btn">Register</button>
+        // </div>
+    )
+}
